7035af6f1c946ccc867c4ab7d3cacba5c35925ba
Paul Beardwell <beardwell@hotmail.com>
2023-11-08 22:31:03 +0000

    refactored worksheet updates

--- run.py
+++ run.py
@@ -65,6 +65,25 @@ def update_sales_worksheet(data):
     sales_worksheet.append_row(data)
     print("Sales worksheet updated succesfuly.\n")
 
+def update_surplus_worksheet(data):
+    """
+    Update the surplus worksheet, add a new row with the data
+    the user inputted.
+    """
+    print("Updating surplus worksheet...\n")
+    surplus_worksheet = SHEET.worksheet("surplus")
+    surplus_worksheet.append_row(data)
+    print("Surplus worksheet updated succesfuly.\n")
+
+def update_worksheet(data, worksheet):
+    """
+    Update the relevant worksheet with a list of integers
+    provided by previous functions
+    """
+    print(f"Updating {worksheet} worksheet...\n")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet} worksheet updated successfully\n")    
 
 def calculate_surplus_data(sales_row):
     """
@@ -86,26 +105,16 @@ def calculate_surplus_data(sales_row):
     return surplus_data
 
 
-def update_surplus_worksheet(surplus_data):
-    """
-    Update the surplus worksheet, add a new row with the data
-    the user inputted.
-    """
-    print("Updating surplus worksheet...\n")
-    surplus_worksheet = SHEET.worksheet("surplus")
-    surplus_worksheet.append_row(surplus_data)
-    print("Surplus worksheet updated succesfuly.\n")
-
 def main():
     """
     Run all program functions
     """
     data = get_sales_data()
     sales_data = [int(num) for num in data]
-    update_sales_worksheet(sales_data)
+    update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
-    update_surplus_worksheet(new_surplus_data)
-    print(new_surplus_data)
+    update_worksheet(new_surplus_data, "surplus")
+    # print(new_surplus_data)
 
 
 print("Welcome to Love Sandwiches Data Automation")
